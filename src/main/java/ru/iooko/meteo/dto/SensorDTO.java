a9c9fa244ea8a4a2fcb2dd3a91c1f6c80ffe6797
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-20 20:54:17 +0300

    SensorDTO

--- src/main/java/ru/iooko/meteo/dto/SensorDTO.java
+++ src/main/java/ru/iooko/meteo/dto/SensorDTO.java
@@ -1,22 +1,13 @@
-package ru.iooko.meteo.models;
+package ru.iooko.meteo.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Table(name = "Sensor")
 @Getter
 @Setter
-public class Sensor {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(name = "name")
+public class SensorDTO {
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 3, max = 30, message = "Sensor name should be between 3 and 30 characters")
     private String name;
