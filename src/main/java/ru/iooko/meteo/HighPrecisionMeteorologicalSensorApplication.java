e7670bf67dd2bb8abd481d9b6564b57abde88594
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-20 18:32:34 +0300

    Create empty Spring Boot project

---
+++ src/main/java/ru/iooko/meteo/HighPrecisionMeteorologicalSensorApplication.java
@@ -0,0 +1,13 @@
+package ru.iooko.meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPrecisionMeteorologicalSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighPrecisionMeteorologicalSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/iooko/meteo/HighPrecisionMeteorologicalSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iooko.meteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighPrecisionMeteorologicalSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
